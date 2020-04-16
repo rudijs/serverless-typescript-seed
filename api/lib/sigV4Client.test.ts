94cefddb430a5dc336eafa46fc25215eff66cb69
Rudi Starcevic <ooly.me@gmail.com>
2020-04-16 11:49:02 +0800

    Updates

--- api/lib/sigV4Client.test.ts
+++ api/lib/sigV4Client.test.ts
@@ -3,11 +3,12 @@ import { sigV4Client } from "./sigV4Client"
 describe("sigV4Client", () => {
   test("should generate signed request headers and url", () => {
     const client = sigV4Client.newClient({
-      accessKey: "AKIAS7XFKMTXXWYT4EE3",
-      secretKey: "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",
+      accessKey: "ACCESSKEY",
+      secretKey: "SECRETKEY",
       sessionToken: undefined,
       endpoint: "https://xe8d25t4od.execute-api.ap-southeast-1.amazonaws.com/dev",
     })
+    //
 
     const signedRequest = client.signRequest({
       method: "GET",
@@ -19,7 +20,7 @@ describe("sigV4Client", () => {
 
     // console.log(signedRequest)
     expect(signedRequest).toHaveProperty("url")
-    expect(signedRequest.headers.Authorization).toMatch(/^AWS4-HMAC-SHA256 Credential=AKIAS7XFKMTXXWYT4EE3.*/)
+    expect(signedRequest.headers.Authorization).toMatch(/^AWS4-HMAC-SHA256 Credential=ACCESSKEY.*/)
     expect(signedRequest.headers).toHaveProperty("Accept")
     expect(signedRequest.headers).toHaveProperty("x-amz-date")
     expect(signedRequest.headers).toHaveProperty("Content-Type")
